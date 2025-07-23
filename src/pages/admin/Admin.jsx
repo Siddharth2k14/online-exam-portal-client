d2dbb4853573703d9b27284d63d00ec552d5bc1f
Siddharth2k14 <siddharthsingh2k@gmail.com>
2025-07-24 00:43:52 +0530

    Chaning the Admin.jsx file

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -4,7 +4,7 @@ import Login from '../../components/Authentication/Login/Login'
 const Admin = () => {
     return (
         <div>
-            <Login name="admin" />
+            <Login />
         </div>
     )
 }
