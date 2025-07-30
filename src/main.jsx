b6f1ece7d9687e8789d5d7c24840efc9418f2123
Siddharth2k14 <siddharthsingh2k@gmail.com>
2025-07-30 16:42:41 +0530

    Adding the SpeedInghts component from vercel in the main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,14 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store.js';
 import { Provider } from 'react-redux';
+import { SpeedInsights } from '@vercel/speed-insights';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
+  <SpeedInsights>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </SpeedInsights>
 );
