546f37c4fbb98b8f41bb5532b97cfd8f024cccaa
Siddharth2k14 <siddharthsingh2k@gmail.com>
2025-08-19 23:45:44 +0530

    Separating the App.jsx based on public routes, admin routes, student routes and shared routes

--- src/main.jsx
+++ src/main.jsx
@@ -19,20 +19,13 @@ import { Provider } from 'react-redux';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Analytics } from "@vercel/analytics/react"
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key')
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Provider store={store}>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <App />
-        <SpeedInsights />
-        <Analytics />
-      </ClerkProvider>
+      <App />
+      <SpeedInsights />
+      <Analytics />
     </Provider>
   </BrowserRouter>
 );
